8eec623433513ed4c20b88892e18af3bc73ac415
349032805@qq.com <349032805@qq.com>
2017-07-07 18:30:35 +0800

    git commit

--- src/views/blogs.vue
+++ src/views/blogs.vue
@@ -77,91 +77,100 @@ import waveBtn from '../components/waveBtn';
 
 <style lang="less" scoped>
 	.blogs{
-        width: 1200px;
-        margin: 0 auto;
-        position: absolute;
-        top: 40px;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        .nav {
-            background-color: rgba(122,193,224,.42);
-            overflow: hidden;
-            border: 1px solid #68adc7;
-            margin: 15px 0;
+    width: 1200px;
+    margin: 0 auto;
+    position: absolute;
+    top: 40px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    .nav {
+        background-color: rgba(122,193,224,.42);
+        overflow: hidden;
+        border: 1px solid #68adc7;
+        margin: 15px 0;
 
-            ul {
-              display: flex;
-              li {
-                flex: 1;
-                cursor: pointer;
-                text-align: center;
-                height: 60px;
-                line-height: 60px;
-                  &.active {
-                    color: #fff!important;
-                    background: #66caf1!important;
-                  }
-                  &:hover {
-                    color: #fff;
-                    background: #66caf1;
-                  }
-
+        ul {
+          display: flex;
+          li {
+            flex: 1;
+            cursor: pointer;
+            text-align: center;
+            height: 60px;
+            line-height: 60px;
+              &.active {
+                color: #fff!important;
+                background: #66caf1!important;
+              }
+              &:hover {
+                color: #fff;
+                background: #66caf1;
               }
 
-            }
+          }
+
         }
+    }
 
-        .content {
-          .column {
-              position: absolute;
-              top: 100px;
-              bottom: 20px;
-              .two-columns {
+      .content {
+        .column {
+            position: absolute;
+            top: 100px;
+            bottom: 20px;
+            .two-columns {
+              height: 100%;
+              .part {
                 height: 100%;
-                .part {
-                  height: 100%;
-                      background-color: rgba(179,234,234,.24);
-                  border: 1px solid #66caf1;
-                  overflow-y: auto;
-                  overflow-x: hidden;
+                background-color: rgba(179,234,234,.24);
+                border: 1px solid #66caf1;
+                overflow-y: auto;
+                overflow-x: hidden;
+              }
+              .left-column {
+                width: 38%;
+                float: left;
+                .intro {
+                  padding: 15px;
+                  text-indent:2em;
+                  text-align: justify;
+                  line-height: 24px;
                 }
-                .left-column {
-                  width: 38%;
-                  float: left;
-                  .intro {
-                    padding: 15px;
-                    text-indent:2em;
-                    text-align: justify;
-                  }
-                }
+              }
 
-                .right-column{
-                  width: 60%;
-                  float: right;
+              .right-column{
+                width: 60%;
+                float: right;
 
-                  .list-box {
-                    padding: 15px;
-                    .box {
-                      width: 100%;
-                      height: 100%;
-                      border: 1px solid #fff;
-                      border-radius: 5px;
-                        ul {
-                          li{
-                            line-height: 43px;
-                            height: 45px;
-                            border-top: 1px solid #fff;
-                            padding: 0 15px;
-                            font-size: 14px;
+                .list-box {
+                  padding: 15px;
+                  .box {
+                    width: 100%;
+                    height: 100%;
+                    border: 1px solid #fff;
+                    border-radius: 5px;
+                      ul {
+                        li{
+                          line-height: 43px;
+                          height: 45px;
+                          border-top: 1px solid #fff;
+                          padding: 0 15px;
+                          font-size: 16px;
+                          transition: .3s;
+                          -webkit-transition: .3s;
+                           &:hover {
+                            color: #fff;
+                            background: #66caf1;
+                            opacity: 0.7;
+                            padding-left: 30px;
                           }
                         }
                       }
-                  }
+                    }
                 }
               }
             }
+          }
 
-        }
       }
+  }
 </style>
